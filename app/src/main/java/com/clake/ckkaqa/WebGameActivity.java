f345cbea738fd8c9fa10b15d109be910e2143071
DESKTOP-671GC55\Whatz <lefthanderomg@gmail.com>
2018-02-12 17:29:54 +0300

    fix back

--- app/src/main/java/com/clake/ckkaqa/WebGameActivity.java
+++ app/src/main/java/com/clake/ckkaqa/WebGameActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.KeyEvent;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
@@ -17,11 +18,13 @@ import coo.clake.ckkaqa.R;
 
 public class WebGameActivity extends Activity {
 
+    private WebView webView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webgame);
+        webView = findViewById(R.id.web_view);
         getUrl();
     }
 
@@ -30,9 +33,26 @@ public class WebGameActivity extends Activity {
         onReceice(url);
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     private void onReceice(String url) {
-        WebView webView = findViewById(R.id.web_view);
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
